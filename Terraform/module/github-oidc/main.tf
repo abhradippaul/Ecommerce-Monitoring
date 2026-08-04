d8d085cce7057437e85aa2e5e3ecf1ceb1fbfe5f
Abhradip Paul <abhradippaul@gmail.com>
2026-08-04 20:30:02 +0530

    feat: expand S3 permissions in OIDC module and update CI workflow dependency graph

--- Terraform/module/github-oidc/main.tf
+++ Terraform/module/github-oidc/main.tf
@@ -70,6 +70,37 @@ resource "aws_iam_policy" "github_actions_ecom_policy" {
       {
         Effect = "Allow"
         Action = [
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:ListBucket",
+          "s3:GetBucketLocation",
+          "s3:GetBucketVersioning",
+          "s3:PutBucketVersioning",
+          "s3:GetEncryptionConfiguration",
+          "s3:PutEncryptionConfiguration",
+          "s3:GetBucketPublicAccessBlock",
+          "s3:PutBucketPublicAccessBlock",
+          "s3:GetBucketCORS",
+          "s3:PutBucketCORS",
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:GetBucketTagging",
+          "s3:PutBucketTagging"
+        ]
+        Resource = "arn:aws:s3:::ecommerce-monitoring-*-s3-bucket"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::ecommerce-monitoring-*-s3-bucket/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
           "iam:GetOpenIDConnectProvider",
           "iam:CreateOpenIDConnectProvider",
           "iam:DeleteOpenIDConnectProvider",
